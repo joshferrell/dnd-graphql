243408a46debb7ceceada29ae573a64365a6cbe9
Joshua Ferrell <josh@joshferrell.me>
2017-05-26 19:55:49 -0700

    feat: create language type

--- src/schema/resolvers/index.js
+++ src/schema/resolvers/index.js
@@ -2,3 +2,4 @@ export { default as SpellResolver } from './SpellResolver';
 export { default as AbilityScoreResolver } from './AbilityScoreResolver';
 export { default as SkillResolver } from './SkillResolver';
 export { default as ProficiencyResolver } from './ProficiencyResolver';
+export { default as LanguageResolver } from './LanguageResolver';
--- src/schema/types/index.js
+++ src/schema/types/index.js
@@ -2,3 +2,4 @@ export { default as SpellType } from './SpellType';
 export { default as AbilityScoreType } from './AbilityScoreType';
 export { default as SkillType } from './SkillType';
 export { default as ProficiencyType } from './ProficiencyType';
+export { default as LanguageType } from './LanguageType';
